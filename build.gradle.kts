13cd54776770f73973085a23b9cf5cfbbc04eb10
Константин Охотников <arbokboe@si-h3.kzn.21-school.ru>
2025-12-01 13:06:48 +0300

    Добавлена зависимость spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,15 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
     compileOnly("org.projectlombok:lombok")
+
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     runtimeOnly("org.postgresql:postgresql")
 }
 
--- src/main/java/com/example/service/BookService.java
+++ src/main/java/com/example/service/BookService.java
@@ -10,11 +10,7 @@ import com.example.repository.BookRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
