092d86151737e643087b178a2551e28d5530757b
konst <Konstant995@ya.ru>
2025-12-03 16:28:56 +0300

    Первый Unit-тест с JUnit 5

---
+++ Module9/src/main/java/com/example/datasource/model/Book.java
@@ -0,0 +1,39 @@
+package com.example.datasource.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.PERSIST
+    )
+    @JoinColumn(name = "author_id")
+    private Author author;
+    @Column(name = "year")
+    private int public_year;
+
+    public Book() {
+    }
+
+    public Book(String title, Author author, int public_year) {
+        this.title = title;
+        this.author = author;
+        this.public_year = public_year;
+    }
+
+    public Book(Long id, String title, Author author, int public_year) {
+        this.title = title;
+        this.author = author;
+        this.public_year = public_year;
+        this.id = id;
+    }
+}
