56d65149a4cc470a5f483368270e7e1706eee306
konst <Konstant995@ya.ru>
2025-12-08 10:39:03 +0300

    Интеграционное тестирование контроллера (@WebMvcTest) и Тестирование POST-запроса с @WebMvcTest

--- Module9/src/main/java/com/example/web/dto/BookResponseDto.java
+++ Module9/src/main/java/com/example/web/dto/BookResponseDto.java
@@ -2,11 +2,13 @@ package com.example.web.dto;
 
 
 import com.example.datasource.model.Book;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BookResponseDto {
     private String title;
     private int public_year;
