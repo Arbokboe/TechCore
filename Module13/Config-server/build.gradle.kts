d93515ff2368bfc151aca57f03ddc69bef8648ba
konst <Konstant995@ya.ru>
2026-01-13 12:40:21 +0300

    Config Server и Eureka + Эссе

--- Module13/Config-server/build.gradle.kts
+++ Module13/Config-server/build.gradle.kts
@@ -22,6 +22,8 @@ extra["springCloudVersion"] = "2025.0.1"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-config-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
